6d8c65f12dc038d311cbc2a1e31383129c561a43
Aníbal Portero Hermida <anibal.portero@pantacor.com>
2021-03-12 09:24:15 +0100

    add atom.mk for internal dependency

---
+++ atom.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := picohttpparser
+LOCAL_DESCRIPTION := HTTP parser library
+
+LOCAL_LDFLAGS := --static
+
+LOCAL_SRC_FILES := picohttpparser.c
+
+LOCAL_INSTALL_HEADERS := picohttpparser.h
+
+include $(BUILD_STATIC_LIBRARY)
