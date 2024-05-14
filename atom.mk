a2c71890e87f5117d5291d98beb58714fc617fa3
Alexander Sack <asac@pantacor.com>
2024-05-14 09:45:49 +0000

    switch alchemy to cmake

--- atom.mk
+++ atom.mk
@@ -4,12 +4,5 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := picohttpparser
 LOCAL_DESCRIPTION := HTTP parser library
-LOCAL_MODULE_FILENAME := libpicohttpparser.a
 
-LOCAL_LDFLAGS := --static
-
-LOCAL_SRC_FILES := picohttpparser.c
-
-LOCAL_INSTALL_HEADERS := picohttpparser.h:usr/include/picohttpparser.h
-
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_CMAKE)
